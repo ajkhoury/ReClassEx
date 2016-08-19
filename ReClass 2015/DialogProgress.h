c9904c4a60201acb92456bd907d83e8eee8dcc87
dude719 <blood_099@hotmail.com>
2016-08-19 00:39:18 -0300

    Added crappy progress dialog for symbol loading

---
+++ ReClass 2015/DialogProgress.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "afxwin.h"
+#include "afxcmn.h"
+
+
+// DialogProgress dialog
+
+class CDialogProgress : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDialogProgress)
+
+public:
+	CDialogProgress(CWnd* pParent = NULL);   // standard constructor
+	virtual ~CDialogProgress();
+
+// Dialog Data
+	enum { IDD = IDD_DIALOG_PROGRESS };
+
+	BOOL SetProgress(int pos);
+
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	virtual BOOL OnInitDialog();
+
+	DECLARE_MESSAGE_MAP()
+
+private:
+	CProgressCtrl m_progressBar;
+
+};
