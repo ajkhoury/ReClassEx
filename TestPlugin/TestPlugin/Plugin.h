a1571ed8d644c8ebcf6ed9e9fbe5926c7c6b301b
dude719 <blood_099@hotmail.com>
2016-11-18 01:18:39 -0400

    Plugin update

--- TestPlugin/TestPlugin/Plugin.h
+++ TestPlugin/TestPlugin/Plugin.h
@@ -1,6 +1,14 @@
 #pragma once
 
-#include "stdafx.h"
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+#include <memory>
+
+#include "resource.h"
+#include "ReClassAPI.h"
 
 BOOL WINAPI WriteCallback( LPVOID write_address, LPVOID buffer_ptr, SIZE_T write_size, PSIZE_T num_write );
 BOOL WINAPI ReadCallback( LPVOID read_address, LPVOID buffer_ptr, SIZE_T read_size, PSIZE_T num_read );
