d5bac9a38c1378b7b9d1d9c87e2e82e84967d4b4
dude719 <blood_099@hotmail.com>
2017-03-06 13:47:14 -0400

    RECLASS_PLUGIN_INFO aligned

--- ReClass/PluginAPI.h
+++ ReClass/PluginAPI.h
@@ -14,8 +14,8 @@ typedef BOOL( PLUGIN_CC *tReadMemoryOperation )(IN LPVOID Address, IN LPVOID Buf
 typedef BOOL( PLUGIN_CC *tWriteMemoryOperation )(IN LPVOID Address, IN LPVOID Buffer, IN SIZE_T Size, OUT PSIZE_T BytesWritten);
 typedef HANDLE( PLUGIN_CC *tOpenProcessOperation )(IN DWORD dwDesiredAccess, IN BOOL bInheritHandle, IN DWORD dwProcessID);
 typedef HANDLE( PLUGIN_CC *tOpenThreadOperation )(IN DWORD dwDesiredAccess, IN BOOL bInheritHandle, IN DWORD dwThreadID);
-DECLSPEC_CACHEALIGN
-typedef struct _RECLASS_PLUGIN_INFO
+
+typedef DECLSPEC_ALIGN( 16 ) struct _RECLASS_PLUGIN_INFO
 {
 	wchar_t Name[256];
 	wchar_t Version[256];
@@ -28,7 +28,7 @@ VOID PLUGIN_CC PluginStateChange( BOOL state );
 typedef BOOL( PLUGIN_CC *tPluginInit )(PRECLASS_PLUGIN_INFO lpRCInfo);
 typedef VOID( PLUGIN_CC *tPluginStateChange )(BOOL state);
 
-typedef DECLSPEC_ALIGN(16) struct _RECLASS_PLUGIN
+typedef DECLSPEC_ALIGN( 16 ) struct _RECLASS_PLUGIN
 {
 	RECLASS_PLUGIN_INFO Info;
 	wchar_t FileName[MAX_PATH];
